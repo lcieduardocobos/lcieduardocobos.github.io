fdf7a771196a6240cb33ecd737bc94127054b175
lcieduardocobos <eduardocobosqa@gmail.com>
2026-04-15 14:12:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-let currentLang = 'es';
-
-function applyLanguage(lang) {
-  document.querySelectorAll('[data-es]').forEach(el => {
-    const text = el.getAttribute(`data-${lang}`);
-    if (text) el.textContent = text;
-  });
-}
-
-function toggleLang() {
-  currentLang = currentLang === 'es' ? 'en' : 'es';
-  applyLanguage(currentLang);
-
-  // Cambia botón
-  const btn = document.querySelector('.lang-btn');
-  btn.textContent = currentLang === 'es' ? 'EN' : 'ES';
-}
-
 function sendEmail() {
-  window.location.href = "mailto:tuemail@email.com";
+  window.location.href = "mailto:eduardocobosv@gmail.com";
 }
 
 function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
-
 /* 🔥 CLAVE: ejecutar cuando TODO esté cargado */
 window.onload = () => {
   applyLanguage('es');
