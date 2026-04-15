9f0b23d0dde28704079e7f9926da5f987ecba089
lcieduardocobos <eduardocobosqa@gmail.com>
2026-04-15 11:49:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let currentLang = 'es';
+
+function toggleLang() {
+  currentLang = currentLang === 'es' ? 'en' : 'es';
+  document.querySelectorAll('[data-es]').forEach(el => {
+    el.textContent = el.getAttribute(`data-${currentLang}`);
+  });
+}
+
 function sendEmail() {
   window.location.href = "mailto:tuemail@email.com";
 }
